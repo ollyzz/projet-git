155664cf2dc339b4f2fb448f1c4425719fc7fddd
ollyzz <ollytay1011@gmail.com>
2026-04-13 15:30:13 +0200

    seperation qualite et e2e

--- qualitecode/tests/test_majuscules.js
+++ qualitecode/tests/test_majuscules.js
@@ -5,12 +5,9 @@ fixture `Test Formulaire Majuscules`
 
 test('Saisir un nom et vérifier la conversion en majuscules', async t => {
     await t
-        // 1. On tape le nom en minuscules
         .typeText('#nom-input', 'tony stark')
         
-        // 2. On clique sur le bouton
         .click('#valider-btn')
 
-        // 3. On vérifie que l'élément d'affichage contient bien "TONY STARK"
         .expect(Selector('#affichage-nom').innerText).eql('TONY STARK');
 });
